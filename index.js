1251d343df18233f5e91fd2de3d6fe7501fef91d
leveragex1 <leveragex502@gmail.com>
2026-03-19 20:09:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ const allowedOrigins = [
   //'https://leveragex-2hpp.onrender.com',
   //'https://leveragex-ejtm.onrender.com', 
   //'https://leveragex-o2kw.onrender.com',
-  'https://lx-frontend-gzve.onrender.com',// Local development environment
+  // 'https://lx-frontend-gzve.onrender.com',
+  'https://leveragex-n64p.onrender.com', // Local development environment
 ];
 
 const corsOptions = {

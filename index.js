055656ae362537efade015d561db06482f8c3dc0
leveragex1 <leveragex502@gmail.com>
2026-04-09 12:27:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ const allowedOrigins = [
   // 'https://lx-frontend-gzve.onrender.com',
   //'https://leveragex-n64p.onrender.com',
   //'https://leveragex-xnan.onrender.com'
-  'https://leveragex-mpqr.onrender.com',// Local development environment
+  //'https://leveragex-mpqr.onrender.com'
+  'https://leveragex-nl0b.onrender.com',// Local development environment
 ];
 
 const corsOptions = {
